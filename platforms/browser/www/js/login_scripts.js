b865ee88e3781f15be9aab7453e9038c7dd85e4d
Kumail Mehdi <kumailmehdi3@gmail.com>
2019-04-26 11:40:53 +0500

    update
    update

--- platforms/browser/www/js/login_scripts.js
+++ platforms/browser/www/js/login_scripts.js
@@ -1,10 +1,15 @@
 jQuery(document).ready(function($){
 	// alert("dasd");
-	
+	if (window.localStorage["n"] != null) {
+					   window.location = "user_data.html";
+					    }
+
 	$('#loginForm').on('submit', function(event){
 		event.preventDefault();
 		$("#submit",this).attr("disabled","disabled");
-
+if (window.navigator.onLine == false) {
+				 	alert("Please Chek Your Internet Connection.");
+				}
         var n = $("#n", this).val();
         var p = $("#p", this).val();
 
